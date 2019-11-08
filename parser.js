cac455af89c2ad30db64660d3c507ef9470af5bb
ShawnLin <45999699+shawnlin0201@users.noreply.github.com>
2019-11-08 18:04:35 +0800

    Update parser.js

--- parser.js
+++ parser.js
@@ -11,47 +11,13 @@ function symbolParser (source = '', splitSymbol = '/', filterSymbol = '_', rever
     return parseStorage
 }
 
-function symbolAntiParser (source, splitSymbol = '/', filterSymbol = '_', reverseParseResult = false, removeLastSymbol = true) {
+function symbolAntiParser (source, splitSymbol = '/', filterSymbol = '_', reverseParseResult = false, removefirstSymbol = false) {
     let stringStorage = ''
     for (key in source) {
         stringStorage += reverseParseResult
             ?`${splitSymbol}${source[key]}${filterSymbol}${key}`
             :`${splitSymbol}${key}${filterSymbol}${source[key]}`
     }
-    return stringStorage
+    
+    return (removefirstSymbol) ? stringStorage.slice(-(stringStorage.length-1)) : stringStorage
 }
-
-// let url = [
-//     '/新北市_city/樹林區_country/鎮前街_street',
-//     'firstname=Shawn&lastname=Lin&Age=25',
-//     '/1-5_week/0830-1000_startwork/1800-1930_endwork'
-// ]
-
-
-// let test = [
-//     symbolParser(url[0], '/', '_'),
-//     symbolParser(url[1], '&', '=', true),
-//     symbolParser(url[2], '/', '_')
-// ]
-// console.group('---symbolParse test---')
-// for(let i = 0; i < test.length; i++) {
-//     console.group('Test: ' + (i+1))
-//     console.log(test[i])
-//     console.groupEnd('Test: ' + (i+1))
-// }
-// console.groupEnd('---symbolParse test---')
-
-
-// let antiTest = [
-//     symbolAntiParser(test[0], '/' ,'_' , true),
-//     symbolAntiParser(test[1], '&' ,'='),
-//     symbolAntiParser(test[2], '/' ,'_', true)
-// ]
-
-// console.group('---symbolAntiParse test---')
-// for(let i = 0; i < antiTest.length ; i++) {
-//     console.group('AntiTest: ' + (i+1))
-//     console.log(antiTest[i])
-//     console.groupEnd('AntiTest: ' + (i+1))
-// }
-// console.groupEnd('---symbolAntiParse test---')
