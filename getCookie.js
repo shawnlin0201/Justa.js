a0e73165175554b5d32f19a7f814970935f33e3f
ShawnL <shawnlin0201@gmail.com>
2019-12-11 18:28:06 +0800

    add cookies fn

---
+++ getCookie.js
@@ -0,0 +1,8 @@
+export default function getCookie (cname) {
+        let value = "; " + document.cookie;
+        let parts = value.split("; " + cname + "=");
+        if (parts.length == 2) 
+    return parts.pop().split(";").shift();
+}
+
+// module.exports = getCookie;
