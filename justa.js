ae648bc5e91fa9972bf3a9042739274c1e2a175b
ShawnL <45999699+shawnlin0201@users.noreply.github.com>
2020-10-29 15:22:25 +0800

    Update justa.js

--- justa.js
+++ justa.js
@@ -1,77 +1,25 @@
-let just = justa = (function(){
-    let
-    symbolParser = function (source = '', splitSymbol = '/', filterSymbol = '_', reverseParseResult = false) {
-        let parseStorage = {}
-        let reverseResult = reverseParseResult ? [0,1] : [1,0]
-        source
-            .split(' ')
-            .join('')
-            .split(splitSymbol)
-            .filter(req => req.indexOf(filterSymbol) > -1)
-            .forEach(req => {
-                let parse = req.split(filterSymbol)
-                parseStorage[parse[reverseResult[0]]] = parse[reverseResult[1]]
-            })
-        return parseStorage
-    },
-    symbolAntiParser = function (source, splitSymbol = '/', filterSymbol = '_', reverseParseResult = false, removeFirstSymbol = false) {
-        let stringStorage = ''
-        for (key in source) {
-            stringStorage += reverseParseResult
-                ?`${splitSymbol}${source[key]}${filterSymbol}${key}`
-                :`${splitSymbol}${key}${filterSymbol}${source[key]}`
+const Justa = (function() {
+    let cache;
+    return function(){
+        if(typeof cache === 'Object') return cache
+
+        this.getCookie = (cname) => {
+            let value = "; " + document.cookie;
+            let parts = value.split("; " + cname + "=");
+            if (parts.length == 2) 
+            return parts.pop().split(";").shift();
         }
-        
-        return (removeFirstSymbol) ? stringStorage.slice(-(stringStorage.length-1)) : stringStorage
-    },
-    getCookie = function (cname) {
-        let value = "; " + document.cookie;
-        let parts = value.split("; " + cname + "=");
-        if (parts.length == 2) return parts.pop().split(";").shift();
-    },
-    setCookie = function (cname, cvalue, exdays){
-        let d = new Date()
-        d.setTime(d.getTime() + (exdays*24*60*60*1000))
-        let expires = `expires=${d.toUTCString()}`
-        document.cookie = `${cname}=${cvalue};${expires}`
-    },
-    deleteCookie = function (cname) {
-        document.cookie = cname + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    },
-    throttle = function (fn, delay) {
-        let coldown = false;
-        let event = null;
-        return function(...args) {
-            const context = this
-            if(coldown) { return }
-            
-            coldown = true
-            clearTimeout(event)
-            event = setTimeout(() => {
-                fn.apply(context, args)
-                coldown = false
-            }, delay)
+    
+        this.setCookie = (cname, cvalue, exdays) => {
+            let d = new Date()
+            d.setTime(d.getTime() + (exdays*24*60*60*1000))
+            let expires = `expires=${d.toUTCString()}`
+            return document.cookie = `${cname}=${cvalue};${expires}`
         }
-    },
-    debounce = function (fn, delay) {
-        let event = null;
-        return function(...args) {
-            const context = this
-            if(event) {
-                clearTimeout(event)
-            }
-            event = setTimeout(() => {
-                fn.apply(context, args)
-            }, delay)
+    
+        this.deleteCookie = (cname) => {
+            return  document.cookie = cname + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
         }
-    }
-    ;return {
-        symbolParser,
-        symbolAntiParser,
-        getCookie,
-        setCookie,
-        deleteCookie,
-        throttle,
-        debounce,
+        cache = this
     }
 })()
